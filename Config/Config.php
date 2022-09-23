c4203aab25d0de23e78368b11312640cd4b79f57
Reynaldo <pr19004@ues.edu.sv>
2022-09-23 13:24:35 -0600

    Modificacion del config para subir al hosting

--- Config/Config.php
+++ Config/Config.php
@@ -24,12 +24,12 @@ const DB_CHARSET = "charset=utf8";
 
 
 /*
-//Datos para la conexion a base de datos
+//Datos para la conexion a base de datos externa
 
-const DB_HOST = "localhost";
-const DB_NAME = "db_tiendavirtual";
-const DB_USER = "root";
-const DB_PASSWORD = "";
+const DB_HOST = "us-cdbr-east-06.cleardb.net";
+const DB_NAME = "heroku_79fb5a24eba3054";
+const DB_USER = "b851d76bdd4c24";
+const DB_PASSWORD = "da646ecc";
 const DB_CHARSET = "charset=utf8";*/
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
